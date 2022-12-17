2886d1a4b05952bf3278791b89652c5796c95200
vinnyreact <119421047+vinnyreact@users.noreply.github.com>
2022-12-17 14:35:48 -0300

    Update

--- app/src/components/pages/Home/index.tsx
+++ app/src/components/pages/Home/index.tsx
@@ -1,19 +1,8 @@
-import { useState } from "react"
-
 function Home(){
-
-  const [name, setName] = useState([]);
-
-  function submit(e: Event){
-    e.preventDefault();
-  }
-
   return(
-    <form>
-      <input type="text" name="name" onChange={(e: Event) => setName({...name, [e.target.value]: e.name.value})} placeholder="Name"/>
-      <input type="submit" value="Enviar"/>
-    </form>
+    <input type="text" placeholder="name"/>
   )
 }
+
 
 export default Home;
