d20f84db94c2c2cf65563ae03d55d064b94a252a
vinnyreact <119421047+vinnyreact@users.noreply.github.com>
2022-12-17 12:37:57 -0300

    Update

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,8 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./components/pages/Home";
 
 
 function App() {
   return (
-    <h1>Hllo World</h1>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+      </Routes>
+    </Router>
   );
 }
 
