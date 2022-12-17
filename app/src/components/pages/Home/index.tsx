a0dc38b25370e5bf24405a4a6ac9f7392fc47957
vinnyreact <119421047+vinnyreact@users.noreply.github.com>
2022-12-17 14:04:26 -0300

    1 file modified

--- app/src/components/pages/Home/index.tsx
+++ app/src/components/pages/Home/index.tsx
@@ -1,6 +1,28 @@
+import { useState } from "react"
+
 function Home(){
+
+  const [name, setName] = useState([]);
+
+  function handleChange(e: string){
+    setName({...name, [e.target.value: e.name.value]})
+  }
+
+  function setLocalStorage(name: string){
+    localStorage.setItem("ticket", JSON.stringify(name));
+  }
+
+  function submit(e: Event){
+    e.preventDefault();
+    setLocalStorage(name)
+    alert(localStorage.getItem("ticket"));
+  }
+
   return(
-    <h1>Home</h1>
+    <form onSubmit={submit}>
+      <input type="text" name="name" onChange={handleChange} placeholder="Name"/>
+      <input type="submit" value="Enviar"/>
+    </form>
   )
 }
 
