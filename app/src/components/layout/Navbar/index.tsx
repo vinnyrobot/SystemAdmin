6ea99971ee47c55c607d20e0d2ff97086a79092e
vinnyrobot <119421047+vinnyrobot@users.noreply.github.com>
2023-01-09 12:32:40 -0300

    Update

--- app/src/components/layout/Navbar/index.tsx
+++ app/src/components/layout/Navbar/index.tsx
@@ -3,35 +3,46 @@ import { Link } from "react-router-dom";
 import styles from "./index.module.css";
 
 import { FaBars } from "react-icons/fa";
+import { useState } from "react";
 
 function Navbar(){
 
+    const [showNavbarResponsive, setShowNavbarResponsive] = useState(false);
+
     function clearLocalStorage(){
         localStorage.clear()
-        alert("Seu token de acesso fou excluído");
+        alert("Seu token de acesso foi excluído");
+    }
+
+    function handleShowNavbarResposive(){
+        setShowNavbarResponsive(true);
     }
 
     return(
-        <section className={styles.navbar}>
-            <div className={styles.logo}>
-                <h2 className={styles.textLogo}>Jvn</h2>
-            </div>
-            <ul className={styles.links}>
-                <li className={styles.link}>
-                    <Link to="/">Home</Link>
-                </li>
-                <li className={styles.link}>
-                    <Link to="/dashboard">Dashboard</Link>
-                </li>
-                <li className={styles.link}>
-                    <Link to="/clients">Clients</Link>
-                </li>
-                <li className={styles.link} onClick={clearLocalStorage}>
-                    <Link to="/">Log Out</Link>
-                </li>
-            </ul>
-            <FaBars size={30}/>
-        </section>
+        <>
+            <header className={showNavbarResponsive ? styles.navbar : styles.closeNavbar}>
+                <div className={styles.logo}>
+                    <h2 className={styles.textLogo}>Jvn</h2>
+                </div>
+                <ul className={styles.links}>
+                    <li className={styles.link}>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li className={styles.link}>
+                        <Link to="/dashboard">Dashboard</Link>
+                    </li>
+                    <li className={styles.link}>
+                        <Link to="/clients">Clients</Link>
+                    </li>
+                    <li className={styles.link} onClick={clearLocalStorage}>
+                        <Link to="/">Log Out</Link>
+                    </li>
+                </ul>
+                <FaBars size={30} onClick={handleShowNavbarResponsive}/>
+            </header>
+            <section className={showNavbarResponsive ? styles.responsiveNavbar : closeResponsiveNavbar}>
+            </section>
+        </>
     )
 }
 
